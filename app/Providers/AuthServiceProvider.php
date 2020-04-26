c5a9da4840298dc3141cfe514e534296367311fd
Aleksey <aleksej.nosko@mail.ru>
2020-04-26 16:00:10 +0300

    Исправлена ошибка в serviceProvider, из-за отсутствующей таблици, при развертывании проекта

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Permission;
 use App\PermissionRole;
 use App\UserRole;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -35,20 +36,22 @@ class AuthServiceProvider extends ServiceProvider
         /*
          * Проверка на наличие разрешения у пользователя.
          */
-        $permissions = Permission::all();
-        foreach ($permissions as $permission){
-            $permission = $permission['name'];
-            Gate::define($permission, function ($user) use ($permission) {
-                $user_role = UserRole::where('user_id', $user->id)->first();
-                if (PermissionRole::with('getPermission')->where('role_id', $user_role['role_id'])->whereHas('getPermission',
-                    function ($query) use ($permission) {
-                        $query->where('name', $permission);
-                    })->exists()) {
-                    return true; // если в таблице permissions находиться данное разр. у польз., то true
-                } else {
-                    return false;
-                }
-            });
+        if (Schema::hasTable('permissions')) {
+            $permissions = Permission::all();
+            foreach ($permissions as $permission){
+                $permission = $permission['name'];
+                Gate::define($permission, function ($user) use ($permission) {
+                    $user_role = UserRole::where('user_id', $user->id)->first();
+                    if (PermissionRole::with('getPermission')->where('role_id', $user_role['role_id'])->whereHas('getPermission',
+                        function ($query) use ($permission) {
+                            $query->where('name', $permission);
+                        })->exists()) {
+                        return true; // если в таблице permissions находиться данное разр. у польз., то true
+                    } else {
+                        return false;
+                    }
+                });
+            }
         }
     }
 }
